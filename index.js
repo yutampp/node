60870e0a4976f10582239428e46473c77ca41a15
yutampp <honntou@hotmail.com>
2021-05-04 18:44:18 +0900

    sh_backup

--- index.js
+++ index.js
@@ -18,6 +18,38 @@ const options = {
 
 process.chdir(__dirname + "/");
 
+const target = "12860"
+
+fs.readFile("deckarray.out", {encoding:"utf8"} ).then(data=>{
+  const deckarray = JSON.parse(data);
+  const t_deck = deckarray.filter(v=>v.includes(target) );
+  //console.log(t_deck.length);
+  const bunbo = t_deck.length;
+  const opponent = Array.from(new Set(t_deck.flat() ) );
+  //console.log(opponent);
+  const izondo = opponent.map((t,i,s)=>{
+    const bunsi = t_deck.filter(v=>v.includes(t) ).length;
+    return [t,bunsi/bunbo]
+  });
+  const hi_izondo = opponent.map((t,i,s)=>{
+    const tt_deck = deckarray.filter(v=>v.includes(t) );
+    const bunbo2 = tt_deck.length;
+    const bunsi2 = tt_deck.filter(v=>v.includes(target)).length;
+    return [t,bunsi2/bunbo2]
+  });
+  const sougo_izondo = izondo.map((t,i,s)=>{
+    return [t[0], t[1]+hi_izondo[i][1]]
+  });
+  const sorted_sougo_izondo = sougo_izondo.sort((a,b)=>a[1]-b[1] ).map(v=>[`https://www.db.yugioh-card.com/yugiohdb/card_search.action?ope=2&cid=${v[0]}&request_locale=ja`,v[1]]);
+
+  //console.log(izondo);
+  //console.log(hi_izondo);
+  console.log(sorted_sougo_izondo);
+
+})
+
+
+/*
 request(options)
 .then(data=>{
   const links = Array.from(
@@ -51,3 +83,5 @@ request(options)
 }).then(data =>{
   console.log(data,process.cwd(),__dirname);
 });
+
+*/
